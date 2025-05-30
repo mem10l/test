df311f95aafcef539798ffb64cbb6b01008a7857
Your Name <you@example.com>
2025-05-30 12:59:48 +0300

    Changed one thing, but removed some usless things

--- resources/views/component/layout.blade.php
+++ resources/views/component/layout.blade.php
@@ -4,26 +4,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
-    <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
 </head>
 <body>
     <header>
         <nav class="navbar">
             <div class="container">
-                <a href="{{ url('/') }}" class="navbar-brand">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                
+                <a href="/" class="navbar-brand">Laravel</a>
                 <ul class="navbar-nav">
                     <li><a href="{{ route('home') }}">Home</a></li>
                     <li><a href="{{ route('contacts.index') }}">Contacts</a></li>
-                    <li><a href="{{ route('books.index') }}">Books</a></li>
+                    <li><a href="{{ route('books.index') }}">books</a></li>
                 </ul>
             </div>
         </nav>
     </header>
-
     <main class="main-content">
         <div class="container">
             {{ $slot }}
